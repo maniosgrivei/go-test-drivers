04f138bd2cecb5884deca022b1e235ab61686014
MANIOS GRIVEI IT - FZCO <m4niosgr1vei@gmail.com>
2025-07-12 21:43:30 +0200

    step 03

--- customer/customer_test.go
+++ customer/customer_test.go
@@ -14,49 +14,27 @@ const (
 
 func TestRegisterCustomer(t *testing.T) {
 	t.Run("should register a customer with valid data", func(t *testing.T) {
-		r := require.New(t)
+		// Given that
+		ArrangeInternalsNoCustomerIsRegistered(t)
 
-		// Arrange: Set the desired initial state.
-		customers = make([]*Customer, 0)
-		idIndex = make(map[string]*Customer)
-		nameIndex = make(map[string]*Customer)
-		emailIndex = make(map[string]*Customer)
-		phoneIndex = make(map[string]*Customer)
-
-		// Act: Perform an action on the system.
-		request := &RegisterRequest{
+		// When we
+		id, err := ActTryToRegisterACustomer(t, &RegisterRequest{
 			Name:  dfltName,
 			Email: dfltEmail,
 			Phone: dfltPhone,
-		}
+		})
+		// with valid data
 
-		id, err := Register(request)
+		// Then the
+		AssertRegistrationShouldSucceed(t, id, err)
 
-		// Assert: Check the results.
-		r.NoError(err)
-		r.NotEmpty(id)
-
-		// Assert: Check the side-effects.
-		customer := Customer{
+		// And the
+		AssertInternalsCustomerShouldBeProperlyRegistered(t, &Customer{
 			ID:    id,
 			Name:  dfltName,
 			Email: dfltEmail,
 			Phone: dfltPhone,
-		}
-
-		r.Contains(customers, &customer)
-
-		r.Contains(idIndex, id)
-		r.EqualValues(idIndex[id], &customer)
-
-		r.Contains(nameIndex, dfltName)
-		r.EqualValues(nameIndex[dfltName], &customer)
-
-		r.Contains(emailIndex, dfltEmail)
-		r.EqualValues(emailIndex[dfltEmail], &customer)
-
-		r.Contains(phoneIndex, dfltPhone)
-		r.EqualValues(phoneIndex[dfltPhone], &customer)
+		})
 	})
 
 	t.Run("should reject a registration with invalid data", func(t *testing.T) {
@@ -76,33 +54,23 @@ func TestRegisterCustomer(t *testing.T) {
 
 		for _, c := range cases {
 			t.Run(c.Case, func(t *testing.T) {
-				r := require.New(t)
+				// Given the system has a
+				ArrangeInternalsNoCustomerIsRegistered(t)
 
-				// Arrange: Set the desired initial state.
-				customers = make([]*Customer, 0)
-				idIndex = make(map[string]*Customer)
-				nameIndex = make(map[string]*Customer)
-				emailIndex = make(map[string]*Customer)
-				phoneIndex = make(map[string]*Customer)
+				// When we
+				id, err := ActTryToRegisterACustomer(t, c.Request)
+				// with invalid data
 
-				// Act: Perform an action on the system.
-				id, err := Register(c.Request)
+				// Then the
+				AssertRegistrationShouldFailWithErrorAndMessage(t, id, err, ErrValidation, c.FindOnError...)
 
-				// Assert: Check the results.
-				r.Empty(id)
-				r.Error(err)
-				r.ErrorIs(err, ErrValidation)
-
-				for _, e := range c.FindOnError {
-					r.Contains(err.Error(), e)
-				}
-
-				// Assert: Check the side-effects.
-				r.Len(customers, 0)
-				r.Len(idIndex, 0)
-				r.Len(nameIndex, 0)
-				r.Len(emailIndex, 0)
-				r.Len(phoneIndex, 0)
+				// And the
+				AssertInternalsCustomerShouldNotBeRegistered(t, &Customer{
+					ID:    "1",
+					Name:  c.Request.Name,
+					Email: c.Request.Email,
+					Phone: c.Request.Phone,
+				})
 			})
 		}
 	})
@@ -121,154 +89,324 @@ func TestRegisterCustomer(t *testing.T) {
 			{"having same email and phone", &RegisterRequest{"Didi Dada", dfltEmail, dfltPhone}, []string{"duplicated email", "duplicated phone"}},
 		}
 
-		// Arrange: Set the desired initial state.
-		customer := Customer{
-			ID:    "1",
-			Name:  dfltName,
-			Email: dfltEmail,
-			Phone: dfltPhone,
-		}
-
-		customers = []*Customer{&customer}
-		idIndex = map[string]*Customer{"1": &customer}
-		nameIndex = map[string]*Customer{dfltName: &customer}
-		emailIndex = map[string]*Customer{dfltEmail: &customer}
-		phoneIndex = map[string]*Customer{dfltPhone: &customer}
-
 		for _, c := range cases {
 			t.Run(c.Case, func(t *testing.T) {
-				r := require.New(t)
+				// Given that
+				ArrangeInternalsSomeCustomersAreRegistered(t, &Customer{
+					ID:    "1",
+					Name:  dfltName,
+					Email: dfltEmail,
+					Phone: dfltPhone,
+				})
 
-				// Act: Perform an action on the system.
-				id, err := Register(c.Request)
+				// When we
+				id, err := ActTryToRegisterACustomer(t, c.Request)
+				// with duplicated data
 
-				// Assert: Check the results.
-				r.Empty(id)
-				r.Error(err)
-				r.ErrorIs(err, ErrDuplication)
+				// Then the
+				AssertRegistrationShouldFailWithErrorAndMessage(t, id, err, ErrDuplication, c.FindOnError...)
 
-				for _, e := range c.FindOnError {
-					r.Contains(err.Error(), e)
-				}
-
-				// Assert: Check the side-effects.
-				assertAreNotTheSame := func(a *Customer, b *RegisterRequest) {
-					r.False(a.Name == b.Name && a.Email == b.Email && a.Phone == b.Phone)
-				}
-
-				for _, rc := range customers {
-					assertAreNotTheSame(rc, c.Request)
-				}
-
-				if rc, found := nameIndex[c.Request.Name]; found {
-					assertAreNotTheSame(rc, c.Request)
-				}
-
-				if rc, found := emailIndex[c.Request.Email]; found {
-					assertAreNotTheSame(rc, c.Request)
-				}
-
-				if rc, found := phoneIndex[c.Request.Phone]; found {
-					assertAreNotTheSame(rc, c.Request)
-				}
+				// And the
+				AssertInternalsCustomerShouldNotBeRegistered(t, &Customer{
+					ID:    "2",
+					Name:  c.Request.Name,
+					Email: c.Request.Email,
+					Phone: c.Request.Phone,
+				})
 			})
 		}
 	})
 
 	t.Run("should not register the same user twice", func(t *testing.T) {
-		r := require.New(t)
-
-		// Arrange: Set the desired initial state.
-		customer := Customer{
+		// Given the system
+		ArrangeInternalsSomeCustomersAreRegistered(t, &Customer{
 			ID:    "1",
 			Name:  dfltName,
 			Email: dfltEmail,
 			Phone: dfltPhone,
-		}
+		})
 
-		customers = []*Customer{&customer}
-		idIndex = map[string]*Customer{"1": &customer}
-		nameIndex = map[string]*Customer{dfltName: &customer}
-		emailIndex = map[string]*Customer{dfltEmail: &customer}
-		phoneIndex = map[string]*Customer{dfltPhone: &customer}
-
-		request := &RegisterRequest{
+		// When we
+		id, err := ActTryToRegisterACustomer(t, &RegisterRequest{
 			Name:  dfltName,
 			Email: dfltEmail,
 			Phone: dfltPhone,
-		}
+		})
+		// twice
 
-		id, err := Register(request)
+		// Then the
+		AssertRegistrationShouldFailWithErrorAndMessage(t, id, err, ErrDuplication, "duplicated name", "duplicated email", "duplicated phone")
 
-		// Assert: Check the results.
-		r.Empty(id)
-		r.Error(err)
-		r.ErrorIs(err, ErrDuplication)
-
-		for _, e := range []string{"duplicated name", "duplicated email", "duplicated phone"} {
-			r.Contains(err.Error(), e)
-		}
-
-		// Assert: Check the side-effects.
-		r.Len(customers, 1)
-		r.EqualValues(customers[0], &customer)
-
-		r.Len(idIndex, 1)
-		r.Contains(idIndex, customer.ID)
-		r.EqualValues(idIndex[customer.ID], &customer)
-
-		r.Len(nameIndex, 1)
-		r.Contains(nameIndex, customer.Name)
-		r.EqualValues(nameIndex[customer.Name], &customer)
-
-		r.Len(emailIndex, 1)
-		r.Contains(emailIndex, customer.Email)
-		r.EqualValues(emailIndex[customer.Email], &customer)
-
-		r.Len(phoneIndex, 1)
-		r.Contains(phoneIndex, customer.Phone)
-		r.EqualValues(phoneIndex[customer.Phone], &customer)
+		// And
+		AssertInternalsCustomerShouldNotBeDuplicated(t, &Customer{
+			Name:  dfltName,
+			Email: dfltEmail,
+			Phone: dfltPhone,
+		})
 	})
 
 	t.Run("should return a generic system error on failure", func(t *testing.T) {
-		r := require.New(t)
+		// Given the system has
+		ArrangeInternalsNoCustomerIsRegistered(t)
 
-		Init()
+		// And
+		ArrangeInternalsSomethingCausingAProblem(t)
 
-		// Arrange: Set the desired initial state.
-		customers = nil
-		idIndex = nil
-		nameIndex = nil
-		emailIndex = nil
-		phoneIndex = nil
-
-		// Act: Perform an action on the system.
-		request := &RegisterRequest{
+		// When we
+		id, err := ActTryToRegisterACustomer(t, &RegisterRequest{
 			Name:  dfltName,
 			Email: dfltEmail,
 			Phone: dfltPhone,
-		}
+		})
+		// with valid data
 
-		id, err := Register(request)
+		// Them the
+		AssertRegistrationShouldFailWithErrorAndMessage(t, id, err, ErrSystem, "contact support")
 
-		// Assert: Check the results.
-		r.Empty(id)
-		r.Error(err)
-		r.ErrorIs(err, ErrSystem)
-		r.Contains(err.Error(), "contact support")
-
-		// Assert: Check the side-effects.
-		customer := Customer{
+		// And the
+		AssertInternalsCustomerShouldNotBeRegistered(t, &Customer{
 			ID:    "1",
 			Name:  dfltName,
 			Email: dfltEmail,
 			Phone: dfltPhone,
-		}
-
-		r.NotContains(customers, &customer)
-		r.NotContains(idIndex, id)
-		r.NotContains(nameIndex, dfltName)
-		r.NotContains(emailIndex, dfltEmail)
-		r.NotContains(phoneIndex, dfltPhone)
+		})
 	})
+}
+
+//
+// Test Helpers
+//
+//
+
+//
+// Arrange
+
+// ArrangeInternalsNoCustomerIsRegistered initializes the repository to a clean state.
+func ArrangeInternalsNoCustomerIsRegistered(t *testing.T) {
+	t.Helper()
+
+	customers = make([]*Customer, 0)
+	idIndex = make(map[string]*Customer)
+	nameIndex = make(map[string]*Customer)
+	emailIndex = make(map[string]*Customer)
+	phoneIndex = make(map[string]*Customer)
+}
+
+// ArrangeInternalsSomeCustomersAreRegistered populates the repository with the
+// given customers.
+func ArrangeInternalsSomeCustomersAreRegistered(t *testing.T, customer ...*Customer) {
+	t.Helper()
+
+	ArrangeInternalsNoCustomerIsRegistered(t)
+
+	for _, c := range customer {
+		customers = append(customers, c)
+		idIndex[c.ID] = c
+		nameIndex[c.Name] = c
+		emailIndex[c.Email] = c
+		phoneIndex[c.Phone] = c
+	}
+}
+
+// ArrangeInternalsSomethingCausingAProblem the internal state in a way that
+// function calls will results in a system error.
+func ArrangeInternalsSomethingCausingAProblem(t *testing.T) {
+	t.Helper()
+
+	customers = nil
+	idIndex = nil
+	nameIndex = nil
+	emailIndex = nil
+	phoneIndex = nil
+}
+
+//
+// Act
+
+// ActTryToRegisterACustomer registers a customer in the system by acting as a user.
+func ActTryToRegisterACustomer(t *testing.T, request *RegisterRequest) (id string, err error) {
+	t.Helper()
+
+	return Register(request)
+}
+
+//
+// Assert
+
+// AssertRegistrationShouldSucceed asserts that the registration was successful.
+func AssertRegistrationShouldSucceed(t *testing.T, id string, err error) {
+	t.Helper()
+
+	r := require.New(t)
+
+	r.NoError(err)
+	r.NotEmpty(id)
+}
+
+// AssertRegistrationShouldFail asserts that the registration failed.
+func AssertRegistrationShouldFail(t *testing.T, id string, err error) {
+	t.Helper()
+
+	r := require.New(t)
+
+	r.Empty(id)
+	r.Error(err)
+}
+
+// AssertRegistrationShouldFailWithError asserts that the registration failed
+// with the given error.
+func AssertRegistrationShouldFailWithError(t *testing.T, id string, err error, targetError error) {
+	t.Helper()
+
+	AssertRegistrationShouldFail(t, id, err)
+
+	require.ErrorIs(t, err, targetError)
+}
+
+// AssertRegistrationShouldFailWithMessage asserts that the registration failed
+// with the given message(s).
+func AssertRegistrationShouldFailWithMessage(t *testing.T, id string, err error, targetMessage ...string) {
+	t.Helper()
+
+	AssertRegistrationShouldFail(t, id, err)
+
+	for _, msg := range targetMessage {
+		require.Contains(t, err.Error(), msg)
+	}
+}
+
+// AssertRegistrationShouldFailWithErrorAndMessage asserts that the
+// registration failed with the given error and message(s).
+func AssertRegistrationShouldFailWithErrorAndMessage(t *testing.T, id string, err error, targetError error, targetMessage ...string) {
+	t.Helper()
+
+	AssertRegistrationShouldFailWithError(t, id, err, targetError)
+
+	AssertRegistrationShouldFailWithMessage(t, id, err, targetMessage...)
+}
+
+// AssertInternalsCustomerShouldBeProperlyRegistered asserts that the customer
+// is properly registered in the internal data structures.
+func AssertInternalsCustomerShouldBeProperlyRegistered(t *testing.T, customer *Customer) {
+	t.Helper()
+
+	r := require.New(t)
+
+	r.True(sliceContainsCustomer(customers, customer))
+
+	r.Contains(idIndex, customer.ID)
+	r.True(customersAreSame(idIndex[customer.ID], customer))
+
+	r.Contains(nameIndex, customer.Name)
+	r.True(customersAreSame(nameIndex[customer.Name], customer))
+
+	r.Contains(emailIndex, customer.Email)
+	r.True(customersAreSame(emailIndex[customer.Email], customer))
+
+	r.Contains(phoneIndex, customer.Phone)
+	r.True(customersAreSame(phoneIndex[customer.Phone], customer))
+}
+
+// AssertInternalsCustomerShouldNotBeRegistered asserts that the customer is not
+// properly registered in the internal data structures.
+func AssertInternalsCustomerShouldNotBeRegistered(t *testing.T, customer *Customer) {
+	t.Helper()
+
+	r := require.New(t)
+
+	r.False(sliceContainsCustomer(customers, customer))
+	r.False(mapContainsCustomer(idIndex, customer))
+	r.False(mapContainsCustomer(nameIndex, customer))
+	r.False(mapContainsCustomer(emailIndex, customer))
+	r.False(mapContainsCustomer(phoneIndex, customer))
+}
+
+// AssertInternalsCustomerShouldNotBeDuplicated asserts that the customer is not
+// duplicated in the internal data structures. IDs are not compared.
+func AssertInternalsCustomerShouldNotBeDuplicated(t *testing.T, customer *Customer) {
+	t.Helper()
+
+	r := require.New(t)
+
+	r.LessOrEqual(sliceCountCustomeOccurrences(customers, customer), 1)
+	r.LessOrEqual(mapCountCustomeOccurrences(idIndex, customer), 1)
+	r.LessOrEqual(mapCountCustomeOccurrences(nameIndex, customer), 1)
+	r.LessOrEqual(mapCountCustomeOccurrences(emailIndex, customer), 1)
+	r.LessOrEqual(mapCountCustomeOccurrences(phoneIndex, customer), 1)
+}
+
+//
+// Utility Functions
+
+// customersAreSame compares two customers for equality. IDs are not compared.
+func customersAreSame(c1, c2 *Customer) bool {
+	if c1 == c2 {
+		return true
+	}
+
+	if c1.Name != c2.Name {
+		return false
+	}
+
+	if c1.Email != c2.Email {
+		return false
+	}
+
+	if c1.Phone != c2.Phone {
+		return false
+	}
+
+	return true
+}
+
+// sliceCountCustomeOccurrences counts the occurrences of a customer in a slice.
+// IDs are not compared.
+func sliceCountCustomeOccurrences(s []*Customer, customer *Customer) int {
+	var count int
+
+	for _, c := range s {
+		if customersAreSame(c, customer) {
+			count++
+		}
+	}
+
+	return count
+}
+
+// sliceContainsCustomer checks if a slice of customers contains a specific
+// customer. IDs are not compared.
+func sliceContainsCustomer(s []*Customer, customer *Customer) bool {
+	for _, c := range s {
+		if customersAreSame(c, customer) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// mapCountCustomeOccurrences counts the occurrences of a customer in a map.
+// IDs are not compared.
+func mapCountCustomeOccurrences(m map[string]*Customer, customer *Customer) int {
+	var count int
+
+	for _, c := range m {
+		if customersAreSame(c, customer) {
+			count++
+		}
+	}
+
+	return count
+}
+
+// mapContainsCustomer checks if a map of customers contains a specific
+// customer. IDs are not compared.
+func mapContainsCustomer(m map[string]*Customer, customer *Customer) bool {
+	for _, c := range m {
+		if customersAreSame(c, customer) {
+			return true
+		}
+	}
+
+	return false
 }
